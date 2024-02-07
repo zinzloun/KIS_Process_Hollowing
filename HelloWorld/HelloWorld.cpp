5d878e9b46f92e534c9001a07b88e501888cd7df
Filippo *ZinzlouN* Bersani <filobers@protonmail.com>
2024-02-07 17:12:32 +0100

    Create HelloWorld.cpp

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow)
+{
+	MessageBoxA(0, "Ciao mondo!", "Process hollowing", 0);
+
+	return 0;
+}
